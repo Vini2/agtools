7067c6b4257681eed9396f237c2da7759757d5bf
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-19 09:25:54 +1030

    DEV: Update demo

--- demo.py
+++ demo.py
@@ -9,8 +9,21 @@ def main():
         assembly_graph_file, contig_paths_file
     )
 
+    # Total number of vertices
     print(f"Total number of vertices in the assembly graph: {assembly_graph.vcount()}")
+
+    # Total number of edges
     print(f"Total number of edges in the assembly graph: {assembly_graph.ecount()}")
+
+    # Iterate through the contigs
+    for i in range(assembly_graph.vcount()):
+        # Get neighbours of each contig
+        neighbours = assembly_graph.neighbors(i, mode="all")
+
+        # Print ID, contig label and number of neighbours
+        print(
+            assembly_graph.vs[i]["id"], assembly_graph.vs[i]["label"], len(neighbours)
+        )
 
 
 if __name__ == "__main__":
