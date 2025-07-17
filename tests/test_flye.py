5958e9b37adb8597da1e4261762efa4ca1a05796
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2025-07-17 16:06:57 +0930

    TST: add tests and data for flye

---
+++ tests/test_flye.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pathlib
+
+from agtools.assemblers import flye
+
+__author__ = "Vijini Mallawaarachchi"
+__credits__ = ["Vijini Mallawaarachchi"]
+
+DATADIR = pathlib.Path(__file__).parent / "data"
+
+def test_get_contig_graph():
+
+    graph_file = DATADIR / "1Y3B" / "assembly_graph.gfa"
+    contig_paths_file = DATADIR / "1Y3B" / "assembly_info.txt"
+    contigs_file = DATADIR / "1Y3B" / "assembly.fasta"
+
+    contig_graph = flye.get_contig_graph(graph_file, contigs_file, contig_paths_file)
+
+    assert contig_graph.graph.vcount() == 67
+    assert contig_graph.graph.ecount() == 2
+
+    assert len(contig_graph.contig_names) == 67
+
+    assert "contig_6" in contig_graph.contig_names.values()
