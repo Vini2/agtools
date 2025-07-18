6a472f3646f5085eb9fe37ffc07ccf9275b2a5c0
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2025-07-18 13:12:46 +0930

    TST: add tests and data for asqg2gfa

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -83,6 +83,14 @@ def test_agtools_fastg2gfa(runner, tmp_dir):
     assert r.exit_code == 0, r.output
 
 
+def test_agtools_asqg2gfa(runner, tmp_dir):
+    outpath = tmp_dir
+    graph = DATADIR / "ESC" / "default-graph.asqg"
+    args = f"-g {graph} -o {outpath}".split()
+    r = runner.invoke(asqg2gfa, args, catch_exceptions=False)
+    assert r.exit_code == 0, r.output
+
+
 def test_agtools_gfa2fastg(runner, tmp_dir):
     outpath = tmp_dir
     graph = DATADIR / "ESC" / "assembly_graph_with_scaffolds.gfa"
